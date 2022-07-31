56a4ded7f7106be3ee408cb7f135472ffd8df3b3
David Emaye <emayeodavid@gmail.com>
2022-07-31 11:00:45 +0100

    first commit

---
+++ api/routes/orders.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders were Fetced'
+  })
+})
+
+router.post('/', (req, res, next) => {
+  const order = {
+    productId: req.body.productId,
+    quantity: req.body.quantity,
+  }
+  res.status(201).json({
+    message: 'Order was Created',
+    order: order
+  })
+})
+
+
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order Details',
+    OrderId: req.params.orderId
+  })
+})
+
+
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order Deleted',
+    OrderId: req.params.orderId
+  })
+})
+
+
+module.exports = router;
